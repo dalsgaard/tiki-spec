793181538a6f73db2565f10728abf3789a6d226c
Kim Dalsgaard <kim@kimdalsgaard.com>
2017-12-06 05:51:01 +0100

    Initial commit

---
+++ demo/demo.mjs
@@ -0,0 +1,85 @@
+import assert from 'assert';
+import { describe, it, before, run } from '../index.mjs';
+
+function later (value) {
+  return new Promise (resolve => {
+    process.nextTick(() => {
+      resolve(value);
+    });
+  });
+}
+
+class Stack extends Array {
+
+  async empty () {
+    return later(!!(this.length == 0));
+  }
+
+}
+
+describe('A stack', () => {
+  let stack;
+
+  before (() => {
+    stack = new Stack();
+  });
+
+  describe('An empty stack', () => {
+
+    it.should('have a length of 0', () => {
+      assert.equal(0, stack.length);
+    });
+
+    it.should('be empty', async () => {
+      const empty = await stack.empty();
+      assert.equal(true, empty);
+    });
+
+  });
+
+  describe('A stack with one element', () => {
+    const pushed = 42;
+
+    before (() => {
+      stack.push(pushed);
+    });
+
+    it.should('have a length of 1', () => {
+      assert.equal(1, stack.length);
+    });
+
+    it.should('not be empty', async () => {
+      const empty = await stack.empty();
+      assert.equal(false, empty);
+    });
+
+    describe('Popping', () => {
+      
+      it.should('return the last element', () => {
+        assert.equal(pushed, stack.pop());
+      });
+  
+    });  
+
+    describe('After popping', () => {
+      
+      before (() => {
+        stack.pop();
+      });
+
+      it.should('have a length of 0', () => {
+        assert.equal(0, stack.length);
+      });
+  
+    });  
+    
+  });
+
+  describe('A stack with more elements', () => {
+    
+    
+  });
+
+});
+
+run();
---
+++ index.mjs
@@ -0,0 +1,2 @@
+export { run } from './lib/collection.mjs';
+export { describe, it, before, after } from './lib/bdd.mjs';
